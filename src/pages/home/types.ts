c1658f3f65b9c0d26fc05af130198c37ba08ca08
brzrbito <turl06112002@gmail.com>
2025-08-28 05:11:07 +0300

    add: types for home page

---
+++ src/pages/home/types.ts
@@ -0,0 +1,14 @@
+// Re-export all types from components
+export type * from './components/header/types'
+export type * from './components/hero/types'
+export type * from './components/trading/types'
+export type * from './components/conversion/types'
+export type * from './components/about/types'
+export type * from './components/common/types'
+export type * from './hooks/types'
+export type * from './utils/types'
+
+// Home page specific types
+export interface HomePageProps {
+  className?: string
+}
