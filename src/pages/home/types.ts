ffe51921a901bbda094e1f8f9146f69e148b5ad5
brzrbito <turl06112002@gmail.com>
2025-09-06 22:38:30 +0300

    fix: remove unused type exports from home page types

--- src/pages/home/types.ts
+++ src/pages/home/types.ts
@@ -4,8 +4,6 @@ export type * from './components/principes/types'
 export type * from './components/header/types'
 export type * from './components/hero/types'
 export type * from './components/common/types'
-export type * from './hooks/types'
-export type * from './utils/types'
 
 // Home page specific types
 export interface HomePageProps {
