79f291946dc7d806879540d65b9ea382a6e4dcdf
brzrbito <turl06112002@gmail.com>
2025-09-07 21:36:00 +0300

    дебаунс функция

--- src/pages/home/components/common/utils.ts
+++ src/pages/home/components/common/utils.ts
@@ -12,11 +12,13 @@ export const debounce = <T extends (...args: unknown[]) => void>(
   func: T,
   delay: number
 ): ((...args: Parameters<T>) => void) => {
-  let timeoutId: NodeJS.Timeout
+  let timeoutId: number | undefined
   
   return (...args: Parameters<T>) => {
-    clearTimeout(timeoutId)
-    timeoutId = setTimeout(() => func(...args), delay)
+    if (timeoutId !== undefined) {
+      clearTimeout(timeoutId)
+    }
+    timeoutId = window.setTimeout(() => func(...args), delay)
   }
 }
 
